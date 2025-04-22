2e6713a4ad388dc91550d86a3394609b4de1d393
Ranieri <ranieribreunig@gmail.com>
2025-04-22 11:21:08 -0300

    Commit inicial do projeto

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //Responsável pela conexão com o banco de dados, meus dados estão assim
+
+    username: "postgres",
+    password: "1234",
+    database: "postgresAPI",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    
+};
